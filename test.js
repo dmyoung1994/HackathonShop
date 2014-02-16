09bcced399fbb0ca6efcde20c167565d17a6fbc1
Petr Glotov <pglotov@yahoo.com>
2014-02-16 02:26:58 -0800

    fix

--- test.js
+++ test.js
@@ -10,10 +10,15 @@ var config = {
     database: 'ShopNearMe'
 }
 
+sql.connect(config, function(err) {
+});
+
+
 //var sql = require('msnodesql');
 //var conn_str = "Driver={SQL Server Native Client 10.0};Server=tcp:{v598oe4pzx}.database.windows.net,1433;Database={ShopNearMe};Uid={ShopNearMeAdmin};Pwd={Shopnearme14};Encrypt=yes;Connection Timeout=30;";
 
 function Test(res){
+
     var request = new sql.Request();
     request.query('SELECT * FROM keywords', function (err, results) {
          if (err) {
@@ -22,10 +27,10 @@ function Test(res){
              res.end("");
              return;
          }
-         // res.writeHead(200, { 'Content-Type': 'text/plain' });
-         // for (var i = 0; i < results.length; i++) {
-         //     res.write("id: " + results[i].id + " keyword: " + results[i].keyword + " count: " + results[i].count);
-         // }
+         res.writeHead(200, { 'Content-Type': 'text/plain' });
+         for (var i = 0; i < results.length; i++) {
+             res.write("id: " + results[i].id + " keyword: " + results[i].keyword + " count: " + results[i].count);
+         }
          res.end("; Done.");
      });
     //res.send('test html');
