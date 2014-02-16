3b62d4d1d7a4395baf8b27a312d2d47bc4c68888
Petr Glotov <pglotov@yahoo.com>
2014-02-16 02:41:59 -0800

    fix

--- test.js
+++ test.js
@@ -21,20 +21,20 @@ var connection = new sql.Connection(config);
 function Test(res){
 
     var request = new sql.Request(connection);
-    request.query('SELECT * FROM keywords', function (err, results) {
-         if (err) {
-             res.writeHead(500, { 'Content-Type': 'text/plain' });
-             res.write("Got error :-( " + err);
-             res.end("");
-             return;
-         }
-         res.writeHead(200, { 'Content-Type': 'text/plain' });
-         for (var i = 0; i < results.length; i++) {
-//             res.write("id: " + results[i].id + " keyword: " + results[i].keyword + " count: " + results[i].count);
-         }
-         res.end("; Done.");
-     });
-    //res.send('test html');
+    // request.query('SELECT * FROM keywords', function (err, results) {
+    //      if (err) {
+    //          res.writeHead(500, { 'Content-Type': 'text/plain' });
+    //          res.write("Got error :-( " + err);
+    //          res.end("");
+    //          return;
+    //      }
+    //      res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //      for (var i = 0; i < results.length; i++) {
+    //          res.write("id: " + results[i].id + " keyword: " + results[i].keyword + " count: " + results[i].count);
+    //      }
+    //      res.end("; Done.");
+    //  });
+    res.send('test html');
 }
 
 module.exports      = Test;
